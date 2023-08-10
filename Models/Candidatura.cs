83899ee13c3a44147d9e66563839312381be9a68
ELLEN VITORIA DE JESUS SILVA <ellen.vjsilva@senacsp.edu.br>
2023-08-10 11:01:34 -0300

    Conteudo do arquivo candidatura

--- Models/Candidatura.cs
+++ Models/Candidatura.cs
@@ -1,7 +1,18 @@
-﻿namespace conexaoemacao.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace conexaoemacao.Models
 {
+    [Table("Candidaturas")]
     public class Candidatura
     {
-       
+        public int VagaId { get; set; }
+        public int CandidatoId { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string Status { get; set; } = string.Empty;
+        [StringLength(50)]
+
+
     }
 }
