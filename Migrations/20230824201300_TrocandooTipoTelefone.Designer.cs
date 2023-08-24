5949a070e9d758b105e45e7d3f3ee5c19276d96b
ellenvdjsilva <ellenvdjsilva@gmail.com>
2023-08-24 17:20:31 -0300

    fazendo as interações

--- Migrations/20230824201300_TrocandooTipoTelefone.Designer.cs
+++ Migrations/20230824201300_TrocandooTipoTelefone.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using conexaoemacao.Data;
@@ -11,9 +12,10 @@ using conexaoemacao.Data;
 namespace conexaoemacao.Migrations
 {
     [DbContext(typeof(ConexaoEmAcaoContext))]
-    partial class ConexaoEmAcaoContextModelSnapshot : ModelSnapshot
+    [Migration("20230824201300_TrocandooTipoTelefone")]
+    partial class TrocandooTipoTelefone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
